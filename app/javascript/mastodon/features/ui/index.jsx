abb03d5bcc6c9196ef3249b45bce1ada7a4c66ca
On10n-n01nO <dlsgk1228@gmail.com>
2025-12-19 06:50:25 +0900

    Update index.jsx

--- app/javascript/mastodon/features/ui/index.jsx
+++ app/javascript/mastodon/features/ui/index.jsx
@@ -32,6 +32,7 @@ import LoadingBarContainer from './containers/loading_bar_container';
 import ModalContainer from './containers/modal_container';
 import NotificationsContainer from './containers/notifications_container';
 
+import { useLocation } from 'react-router-dom';
 import CustomAudioPlayer from '../../../components/custom_audio_player';
 
 import {
@@ -606,5 +607,23 @@ class UI extends PureComponent {
 
 }
 
+const UI = () => {
+  const location = useLocation();
+
+  return (
+    <div className="ui">
+      {/* 기존 UI 구조 */}
+      <Navigation />
+      <MainContent />
+
+      {/* 오디오 플레이어: 환경설정 페이지에서는 숨김 */}
+      {location.pathname !== '/settings' && (
+        <div className="columns-area__footer">
+          <CustomAudioPlayer src="/sounds/music.mp3" />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default connect(mapStateToProps)(injectIntl(withRouter(UI)));
