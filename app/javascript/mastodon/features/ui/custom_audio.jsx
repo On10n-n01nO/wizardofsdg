3db8be5b680058f8ad0f4420c75043ab1f4626f2
On10n-n01nO <dlsgk1228@gmail.com>
2025-12-19 07:29:44 +0900

    Update custom_audio.jsx

--- app/javascript/mastodon/features/ui/custom_audio.jsx
+++ app/javascript/mastodon/features/ui/custom_audio.jsx
@@ -1,9 +1,10 @@
+// app/javascript/mastodon/features/ui/custom_audio.jsx
 import React from 'react';
-import CustomAudioPlayer from '../../components/custom_audio_player';
+import CustomAudioPlayer from '../../components/custom_audio_player.js';
 
 const CustomAudioUI = () => (
   <div className="custom-audio-footer">
-    <CustomAudioPlayer src="/sounds/music.mp3" />
+    <CustomAudioPlayer src="/sounds/music.mp3" alt="Background music" />
   </div>
 );
 
