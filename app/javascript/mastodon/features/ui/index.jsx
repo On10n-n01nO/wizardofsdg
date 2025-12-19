e7faa9bffcee4e80c02a8b34b775523b1f5b19b6
On10n-n01nO <dlsgk1228@gmail.com>
2025-12-19 09:13:35 +0900

    Update index.jsx

--- app/javascript/mastodon/features/ui/index.jsx
+++ app/javascript/mastodon/features/ui/index.jsx
@@ -31,6 +31,16 @@ import ColumnsAreaContainer from './containers/columns_area_container';
 import LoadingBarContainer from './containers/loading_bar_container';
 import ModalContainer from './containers/modal_container';
 import NotificationsContainer from './containers/notifications_container';
+
+import CustomAudioPlayer from 'mastodon/components/custom_audio_player';
+
+const UI = () => (
+  <>
+    {/* 기존 UI */}
+    <CustomAudioPlayer />
+  </>
+);
+
 import {
   Compose,
   Status,
