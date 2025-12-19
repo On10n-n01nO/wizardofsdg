7345bf45bec1fe54f49bdc6a10ccb7acc0b4a659
On10n-n01nO <dlsgk1228@gmail.com>
2025-12-19 09:47:20 +0900

    Update navigation_panel.jsx

--- app/javascript/mastodon/features/ui/components/navigation_panel.jsx
+++ app/javascript/mastodon/features/ui/components/navigation_panel.jsx
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import { Component } from 'react';
 
 import { defineMessages, injectIntl } from 'react-intl';
-
 import { Link } from 'react-router-dom';
 
 import { WordmarkLogo } from 'mastodon/components/logo';
@@ -14,6 +13,8 @@ import DisabledAccountBanner from './disabled_account_banner';
 import FollowRequestsColumnLink from './follow_requests_column_link';
 import NotificationsCounterIcon from './notifications_counter_icon';
 import SignInBanner from './sign_in_banner';
+
+// ✅ 추가
 import CustomAudioPlayer from 'mastodon/components/custom_audio_player';
 
 const messages = defineMessages({
@@ -31,18 +32,7 @@ const messages = defineMessages({
   openedInClassicInterface: { id: 'navigation_bar.opened_in_classic_interface', defaultMessage: 'Posts, accounts, and other specific pages are opened by default in the classic web interface.' },
 });
 
-<div className='navigation-panel__legal'>
-  <hr />
-  <ColumnLink transparent to='/about' icon='ellipsis-h' text={intl.formatMessage(messages.about)} />
-</div>
-
-{/* 여기 추가 */}
-<CustomAudioPlayer />
-
-<NavigationPortal />
-
 class NavigationPanel extends Component {
-
   static contextTypes = {
     router: PropTypes.object.isRequired,
     identity: PropTypes.object.isRequired,
@@ -66,10 +56,10 @@ class NavigationPanel extends Component {
           <Link to='/' className='column-link column-link--logo'><WordmarkLogo /></Link>
 
           {transientSingleColumn ? (
-            <div class='switch-to-advanced'>
+            <div className='switch-to-advanced'>
               {intl.formatMessage(messages.openedInClassicInterface)}
               {" "}
-              <a href={`/deck${location.pathname}`} class='switch-to-advanced__toggle'>
+              <a href={`/deck${location.pathname}`} className='switch-to-advanced__toggle'>
                 {intl.formatMessage(messages.advancedInterface)}
               </a>
             </div>
@@ -113,11 +103,13 @@ class NavigationPanel extends Component {
           <ColumnLink transparent to='/about' icon='ellipsis-h' text={intl.formatMessage(messages.about)} />
         </div>
 
+        {/* ✅ 여기 추가 */}
+        <CustomAudioPlayer />
+
         <NavigationPortal />
       </div>
     );
   }
-
 }
 
 export default injectIntl(NavigationPanel);
