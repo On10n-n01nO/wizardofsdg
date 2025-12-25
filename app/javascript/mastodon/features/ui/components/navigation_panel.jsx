676a4bef027e1177902093f510f66079cec4c7ab
On10n-n01nO <dlsgk1228@gmail.com>
2025-12-25 15:39:13 +0900

    Update navigation_panel.jsx

--- app/javascript/mastodon/features/ui/components/navigation_panel.jsx
+++ app/javascript/mastodon/features/ui/components/navigation_panel.jsx
@@ -74,25 +74,27 @@ class NavigationPanel extends Component {
             <FollowRequestsColumnLink />
 
 
-        {!location.pathname.startsWith('/settings') &&
-          !location.pathname.startsWith('/preferences') && (
-            <>
-              <audio id="audioPlayer" src="/sounds/music.mp3" loop></audio>
-              <button onClick={this.togglePlay} className="music-button">
-                {this.state.isPlaying ? (
-                  <svg width="20" height="20" viewBox="0 0 24 24" fill="white">
-                    <rect x="6" y="4" width="4" height="16" />
-                    <rect x="14" y="4" width="4" height="16" />
-                  </svg>
-                ) : (
-                  <svg width="20" height="20" viewBox="0 0 24 24" fill="white">
-                    <polygon points="5,3 19,12 5,21" />
-                  </svg>
-                )}
-              </button>
-            </>
-        )}      {/* ← 여기서 괄호 닫기 */}
-
+            {!location.pathname.startsWith('/settings') &&
+              !location.pathname.startsWith('/preferences') && (
+                <>
+                  <audio id="audioPlayer" src="/sounds/music.mp3" loop></audio>
+                  <button onClick={this.togglePlay} className="music-button">
+                    {this.state.isPlaying ? (
+                      <svg width="20" height="20" viewBox="0 0 24 24" fill="white">
+                        <rect x="6" y="4" width="4" height="16" />
+                        <rect x="14" y="4" width="4" height="16" />
+                      </svg>
+                    )  : (
+                      <svg width="20" height="20" viewBox="0 0 24 24" fill="white">
+                        <polygon points="5,3 19,12 5,21" />
+                      </svg>
+                    )}
+                  </button>
+                </>
+            )}      {/* ← 여기서 괄호 닫기 */}
+          </>
+        )}
+        
         {(signedIn) && (
           <ColumnLink transparent to='/public/local' isActive={this.isFirehoseActive} icon='hashtag' text={intl.formatMessage(messages.firehose)} />
         )}
